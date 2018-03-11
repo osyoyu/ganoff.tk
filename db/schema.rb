f965d33672a58a20722c9e2e29e76f0460e4cbab
osyoyu <osyoyu@osyoyu.com>
2018-03-11 14:06:06 +0900

    Update gems

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -14,7 +13,7 @@
 ActiveRecord::Schema.define(version: 20151014031203) do
 
   create_table "scores", force: :cascade do |t|
-    t.string  "name"
+    t.string "name"
     t.integer "score"
   end
 
